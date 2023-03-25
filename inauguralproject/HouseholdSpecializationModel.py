b23ec9dc6e17aa5e1a85fe2b35bc10606a4dc6ba
tjr126 <msoersted@gmail.com>
2023-03-25 13:56:18 +0100

    Koden er blevet fucker god

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -29,8 +29,8 @@ class HouseholdSpecializationModelClass:
         # d. wages
         par.wM = 1.0
         par.wF = 1.0
-        par.wF_vec = np.linspace(0.8,1.2,5)
-
+        par.wF_vec =  np.linspace(0.8,1.2,5)
+        
         # e. targets
         par.beta0_target = 0.4
         par.beta1_target = -0.1
@@ -110,20 +110,15 @@ class HouseholdSpecializationModelClass:
         opt.LF = LF[j]
         opt.HF = HF[j]
 
-    
-        #fig = plt.figure()
-        #ax = fig.add_subplot(1, 1, 1)
-        #ax.plot(HF/HM, u)
-
-        #ax.set_ylim([0, 24])
-        #ax.set_xlim([0, 24])
+            
+     
     
 
 
         # e. print
-        if do_print:
-            for k,v in opt.__dict__.items():
-                print(f'{k} = {v:6.4f}')
+        #if do_print == True:
+         #   for k,v in opt.__dict__.items():
+         #       print(f'{k} = {v:6.4f}')
 
         return opt
 
@@ -131,29 +126,107 @@ class HouseholdSpecializationModelClass:
     def plot_ratios_alpha(self): #Virker mærkeligt - 0.5 er nødt til at være til sidst før den fatter alpha skal være 0.5 fremover (ligesom i baseline modellen)
         """ plots the ratio for different alphas """
         par = self.par
-        alpha_vec = (0.25, 0.75, 0.50)
-    
+        alpha_vec = (0.25, 0.50 , 0.75 )
+        print("Plot the relationship between the ratio of working home between genders and alpha and sigma respectively")
 
         alpha_ratios = [] #initialize empty list
         # loop over the different values for alpha
         for par.alpha in alpha_vec:
             result = self.solve_discrete(par.alpha)
-            
+            ratio = result.HF / result.HM 
+            alpha_ratios.append(ratio)
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(alpha_vec, alpha_ratios)
+        ax.set_ylabel("Female work from home relative to male")
+        ax.set_xlabel("Alpha")
+        ax.set_title("Ratio between working from home dependent on alpha")
+        ax.set_ylim()
+        ax.set_xlim([0.2,0.8])
 
-            
+        par.alpha = 0.5
 
-            #ratio = HF / HM
-
-            #alpha_ratios = alpha_ratios.append(ratio)
-        return result
+        
+    def plot_ratios_sigma(self): #Virker mærkeligt - 0.5 er nødt til at være til sidst før den fatter alpha skal være 0.5 fremover (ligesom i baseline modellen)
+        """ plots the ratio for different alphas """
+        par = self.par
+        sigma_vec = (0.5, 1 , 1.5 )
+        
     
+     
+
+        sigma_ratios = [] #initialize empty list
+        # loop over the different values for alpha
+        for par.sigma in sigma_vec:
+            result = self.solve_discrete(par.sigma)
+            ratio = result.HF / result.HM 
+            sigma_ratios.append(ratio)
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(sigma_vec, sigma_ratios)
+        ax.set_ylabel("Female work from home relative to male")
+        ax.set_xlabel("Sigma")
+        ax.set_title("Ratio between working from home dependent on sigma")
+        ax.set_ylim()
+        ax.set_xlim([0.4,1.6])
+    
+    
+    def plot_logratios_discrete(self): #Virker mærkeligt - 0.5 er nødt til at være til sidst før den fatter alpha skal være 0.5 fremover (ligesom i baseline modellen)
+        """ plots the ratio for different alphas """
+        par = self.par
+     
+
+        log_workratios = [] #initialize empty list
+        log_wageratios = [] #initialize empty list
+
+        # loop over the different values for wF
+        for par.wF in par.wF_vec:
+            result = self.solve_discrete(par.wF)
+            log_workratiosCalc = np.log(result.HF / result.HM)
+            log_workratios.append(log_workratiosCalc)
+            log_wageratiosCalc = np.log(par.wF / par.wM)
+            log_wageratios.append(log_wageratiosCalc)
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(log_wageratios, log_workratios )
+        ax.set_ylabel("log_workratios")
+        ax.set_xlabel("log_wageratios")
+        ax.set_title("Log workratios and log wage ratios when varying female wages")
+        ax.set_ylim()
+        ax.set_xlim([-0.25,0.25])
 
 
+    def solve_cont(self,do_print=False):
+        """ solve model continously """       
+        par = self.par
+        sol = self.sol 
+        
 
-    def solve(self,do_print=False):
-        """ solve model continously """
-
-        pass    
+        u = self.calc_utility(LM,HM,LF,HF)
+    
+     # a. objective function (to minimize) 
+        obj = lambda x: -self.calc_utility() # minimize -> negative of utility
+        
+     # b. constraints and bounds
+        budget_constraint = lambda x:  (LM+HM < 24) | (LF+HF < 24) 
+        constraints = ({'type':'ineq','fun':budget_constraint})
+        bounds = ((1e-8,par.m/par.p1-1e-8),(1e-8,par.m/par.p2-1e-8))
+    
+        # why all these 1e-8? To avoid ever having x1 = 0 or x2 = 0
+    
+     # c. call solver
+        x0 = [(par.m/par.p1)/2,(par.m/par.p2)/2]
+        result = optimize.minimize(obj,x0,method='SLSQP',bounds=bounds,constraints=constraints)
+        
+     # d. save
+     
+        sol.LM_vec = result.LM_vec
+        sol.x2 = result.x[1]
+        sol.u = model.u_func(sol.x1,sol.x2)
+    
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
